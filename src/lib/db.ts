b1dd7db994892c868317fbdc0a202488fff369e2
Rafat Hussain <rafat.hsn@gmail.com>
2025-10-02 21:35:57 +0530

    vercel error

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -8,7 +8,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['query'],
+    log: ['error', 'warn'], // Reduced logging in production
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
